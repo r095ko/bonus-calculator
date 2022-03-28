4894d2d18bbdf44623eff845f9ced0ca29246664
r095ko <r095ko@gmail.com>
2022-03-28 17:09:20 +0300

    fix

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -39,9 +39,9 @@ class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000_60;
+        long amount = 1000_60;
         boolean registered = false;
-        long expected = 500;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
